1cb6334f887e28266649e30d4be1d8c4ee44c561
Tupi Guedes <tupiguedes@outlook.com>
2023-03-05 15:06:47 -0300

    Iniciando projeto

---
+++ src/main/java/com/konthub/kontHub/KontHubApplication.java
@@ -0,0 +1,13 @@
+package com.konthub.kontHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konthub/kontHub/KontHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konthub.kontHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
